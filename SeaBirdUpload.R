ddde4a8f004da4e2ea09ca7306f84e07feeff3f9
annikaputt <annika@instream.net>
2015-11-13 14:41:17 -0800

    Code edits November 13, 2015.

--- SeaBirdUpload.R
+++ SeaBirdUpload.R
@@ -2,52 +2,51 @@
 # SeaBirdUpload.R
 # SeaBirdProcessing.Rproj
 
-# Uploads raw .cnv files and combines
+# Uploads raw .cnv files and combines by station
 # CNV files need to be separated into bins based on their station number S1, A1, ... 
 
 # Created August 31, 2015
 # A Putt
 ############################
 
+library(segmented)
+
 ##########################
 # seabirdfunc
 ##########################
 
 # Create a function to process incoming .cnv files
-# Notes: skip=232 indicates to start reading at line 233 (there is a lot of header in the file)
+# Notes: skip=232 indicates to start reading at line 233 (there is a lot of header in the file that we don't want)
 
 seabirdfunc <- function(station,datafile) {
+  # 1. Basic upload
   getdata <- read.table(sprintf("Data/%s/%s",station,datafile),skip=232,sep="",header=FALSE,
                         col.names=c("scancount","pressure.db","temp.c","cond.us.cm3","turb.ntu","fluor.mg.m3","oxygen.mg","par","flag"))
-  # Add a column with date
-  # Pull the row from the header that has the date
-  getdaterow <- read.csv(sprintf("Data/%s/%s",station,datafile),skip=87,nrow=1,col.names="starttime")
-  getdate <- substr(x=row.names(getdaterow),start=16,stop=35) # Pull the exact digits we need for the date
+  getdaterow <- read.csv(sprintf("Data/%s/%s",station,datafile),skip=87,nrow=1,col.names="starttime")   # Add a column with date by pulling the date row from the .cnv file
+  getdate <- substr(x=row.names(getdaterow),start=16,stop=35) # Pull the exact digits we need for the date from the string
   getdata$starttime <- getdate
-  getdata$date <- substr(x=getdata$starttime,start=1,stop=11)
+  getdata$date <- substr(x=getdata$starttime,start=1,stop=11) # Pull only the date; remove time as this might be useful later
   
+  # 2. Define the surface, cast, and retrieval portions
   # Add a column that details whether the data are from the surface, cast, and retreival
   p.max <- getdata[which(getdata$pressure.db == max(getdata$pressure.db)), ] # Find max pressure, or end of down cast
   # print(p.max)
   # browser()
   downdata <- getdata[1:row.names(p.max)[1],] # Get rid of any data after p.max  
   # Run a piecewise regression to find the point at which the seabird began lowering
-  library(segmented)
-  linear <- lm(scancount~pressure.db,data=downdata) # Run a lm to be put into the segmented call
-  X <- downdata$pressure.db # Pull the X variable; for some reason this makes the segmented call run properly
-  segmented.mod <- segmented(linear, seg.Z = ~X, psi=2) # psi is an estimated breakpoint
+  linear <- lm(scancount~pressure.db,data=downdata) # Run a lm to be put into the segmented function call
+  X <- downdata$pressure.db # Define the X variable; for some reason this makes the segmented call run properly
+  segmented.mod <- segmented(linear, seg.Z = ~X, psi=2) # Run the segmented model; psi is an estimated breakpoint
   break.point <- segmented.mod$psi[2] # Pull the estimated break point of the two models
   cast <- downdata[which(downdata$pressure.db >= break.point), ] # Keep only the data from a higher pressure than the break point
-  surface <- downdata[which(downdata$pressure.db < break.point), ] # Save the equilibriation
+  surface <- downdata[which(downdata$pressure.db < break.point), ] # Save the surface portion
   cast$portion <- "cast"
-  surface$portion <- "surface"
-  
+  surface$portion <- "surface"  
   # Add the retrieval data
-  # browser()
   retrieval <- getdata[(as.numeric(row.names(p.max))[1]+1):nrow(getdata),]
   retrieval$portion <- "retrieval"
   
-  # Combine the data back into one frame
+  # 3. Combine the data back into one frame
   wholecast <- rbind(surface,cast,retrieval)
   return(wholecast)
 }
@@ -56,17 +55,15 @@ seabirdfunc <- function(station,datafile) {
 # runstationsfunc
 ##########################
 
-# Create a function to loop over the seabirdfunc for each station in the project
+# Create a function to loop over the seabirdfunc for each station in the project (represented as sub-folder)
 # This will create a large data file with all the data from each station stored in 1 frame (all dates)
 # Data must be stored in sub-folders in the Data folder labeled by station
 
 runstationsfunc <- function(stationname) {
-  # Find the seabird files and run the function through them.
-  file_path  <- list.files(path=sprintf("Data/%s",stationname), pattern="*.cnv") # Pull all files
+  file_path  <- list.files(path=sprintf("Data/%s",stationname), pattern="*.cnv")   # Pull all of the subfolders from the data folder
   file_list  <- list()
-  
-  # Loop through all files. 
-  for(i in 1:length(file_path)){
+     
+  for(i in 1:length(file_path)){ # Loop through all files.
     tempdata <- file_path[i]
     file_list[[i]] <- seabirdfunc(datafile=tempdata,station=stationname)
   }
@@ -81,9 +78,8 @@ runstationsfunc <- function(stationname) {
 # Run Through Sub-folders
 ##########################
 
-# Run all stations
+# Run all stations through the two nestled functions
 # Returns a data frame of all the year's scans, with a few extra columns added containing date, station, and poriton of the cast
-# !!!!! I should really streamline this and also add code to write the data frames so that the code doesn't need to be re-run each time.
 StationList <- c("S1","S2","S3","S4","S5","S6","A1","A2","A4","C1","C2","C3","C4","C5","C6","C7","C7","C8","C9","C10")
 for (i in 1:length(StationList)) {
   name <- sprintf("%sdata.csv",StationList[i])
--- TemporalPlots.R
+++ TemporalPlots.R
@@ -12,14 +12,19 @@ library(lubridate)
 library(plyr)
 
 ##########################
-# Get the data manipulated in SeabirdUpload.R
+# Read in data
 ##########################
 
+# Get the data manipulated in SeabirdUpload.R
+# I read in the data separately for the averaging functions (from the same folder)
+# Here is read in on it's own for plotting and such
+# e.g., there will be a S1data (alldata) and AveragedS1 (rounded pressures)
+
 # Option 1:
-# Source the upload file 
+# Source the upload file so that all station data frames are in the working directory
 # Right now the actual files are not retained in the workspace b/c they are generated in a function, so you have to run the 
 # upload code and then read the data back in. But this is faster anyway because the upload takes a long time to run.
-source("SeaBirdUpload.R") 
+# source("SeaBirdUpload.R") 
 
 # Option 2:
 # If the upload has been performed and the most current files are in the StationDataCSV sub-folder you can just read those files in
@@ -31,69 +36,33 @@ for(i in 1:length(file_path)){
 }
 
 
-##########################
-# Code tidbits
-##########################
-
-# Code to plot data from an individual cast
-# Creates a new page for each date
-# Subset only the cast (get rid of the retrival and the surface portions
-# Create plots of temp, oxygen, cond, turb, fluor
-
-OneStationPlotFunc <- function(StationDataFrame) { # StationDataFrame e.g., S1data, A1data, etc
-  castonly <- subset(StationDataFrame,portion=="cast")
-  for (i in 1:length(levels(castonly$date))) {
-    cast <- subset(castonly,date==levels(castonly$date)[i])
-    windows()
-    par(mfrow=c(2,3),oma=c(1,1,2,1))
-    plot(cast$temp.c,cast$pressure.db,type="l",ylim=rev(range(cast$pressure.db)),ylab="Pressure dbar", xlab="Temp C")
-    plot(cast$cond.us.cm3,cast$pressure.db,type="l",ylim=rev(range(cast$pressure.db)),ylab="Pressure dbar", xlab="Conductivity")
-    plot(cast$turb.ntu,cast$pressure.db,type="l",ylim=rev(range(cast$pressure.db)),ylab="Pressure dbar", xlab="Turbidity NTU")
-    plot(cast$fluor.mg.m3,cast$pressure.db,type="l",ylim=rev(range(cast$pressure.db)),ylab="Pressure dbar", xlab="Fluorescence mg/m3")
-    plot(cast$oxygen.mg,cast$pressure.db,type="l",ylim=rev(range(cast$pressure.db)),ylab="Pressure dbar", xlab="Oxygen mg/L")
-    mtext(sprintf("%s: %s",cast$station[1],cast$date[1]), side = 3, line=0, outer = TRUE)
-  }
-}
-  
-# Run example
-# Will automatically open new plot windows; one for each date
-OneStationPlotFunc(S1data)
-
 ############################
-# Code to prepare data for heatmaps
-# this represents the code in the start of the seton s04 depth oxy time series plot file
-# Need to clean, automate, and apply to a bunch of data
+# Prepare Data for Heatmaps
+############################
 
-# Turn date into a factor 
-temp <- S1data #for now...
-temp$posixdate <- as.POSIXct(temp$date,format="%b %d %Y")
-temp$pressure.round <- round(temp$pressure,1)
+# Standardize the pressures between all casts and perform linear interpolation to fill in any resulting gaps in desired variables
 
-# subset out each date !!! this will go into a function
-tosubset <- levels(temp$date)
-temp <- subset(temp, date==tosubset[1])
+########################
+# AveragingFunc
+########################
 
-# Find the day number for the date
-daynumber <- yday(temp$posixdate[1])
-#temp$month <- daynumber
+# Standardize all pressures to follow the seq 0.1, 0.2, .... 
+# Any missing variable data will be interpolated linearly from neighbouring data points
+# This function is applied to one column (i.e., variable) from one cast (i.e., date)
 
-###############################################################################
-# Create subsets for each of the variables I'm interested in plotting
-# This will be working on a single cast subsetted from the single day (will be within another function)
-variables <- c("temp.c","turb.ntu","oxygen.mg")
-averagingFunc <- function(column) {
-  # Subset only pressure and the variable of interest and round the pressure
-  avgtemp <- data.frame(pressure.db=temp$pressure.round,variable=temp[,column])
-  # Find the average of the variable for each value of pressure
+averagingFunc <- function(column,data) {
+  avgtemp <- data.frame(pressure.db=data$pressure.round,variable=data[,column]) # Retain pressure (rounded) and the variable of interest
+  # There are many duplicate pressures associated with variable values. Find the average of the variable associated with each pressure.
+  # The resulting frame has only one value of each pressure
   avgtemp.means <- ddply(subset(avgtemp, pressure.db > 1), c("pressure.db"), summarize, pressure.db=mean(pressure.db), variable=round(mean(variable),2))
   # Create a dummy vector of pressure with each 0.1 interval pressure value, then merge with the average data frame
-  dummy.pressure <- data.frame(pressure.db=seq(from=0,to=max(avgtemp.means$pressure.db),by=1))
+  dummy.pressure <- data.frame(pressure.db=seq(from=0,to=max(avgtemp.means$pressure.db),by=0.1))
   avgtemp.dummy <- merge(dummy.pressure,avgtemp.means,all.x=TRUE,by="pressure.db")
-  # Fill in the missing values that the merge created with a linear approximation and add a column for the day of the year
+  # Fill in the missing values that the merge created with a linear approximation and add a column for the day of the year  
+  daynumber <- yday(data$posixdate[1]) # Identify the day number for each cast from the original oneday data frame created in the loop 
   avgfinal <-data.frame(
     approx(avgtemp.dummy$pressure.db, avgtemp.dummy$variable, xout=avgtemp.dummy$pressure.db, method = "linear")[1],
-    approx(avgtemp.dummy$pressure.db, avgtemp.dummy$variable, xout=avgtemp.dummy$pressure.db, method = "linear")[2], 
-    "month"=daynumber)
+    approx(avgtemp.dummy$pressure.db, avgtemp.dummy$variable, xout=avgtemp.dummy$pressure.db, method = "linear")[2],"month"=daynumber)
   names(avgfinal) <- c("pressure.db","variable","month")
   # The linear approximation can leave NAs at the start of the data frame. Fill with the first whole value.
   avgfinal$variable[is.na(avgfinal$variable)] <- avgfinal$variable[max(which(is.na(avgfinal$variable)))+1]
@@ -101,8 +70,108 @@ averagingFunc <- function(column) {
   return(avgfinal)
 }
 
-temp.temp <- averagingFunc("temp.c")
-temp.turb <- averagingFunc("turb.ntu")
-temp.ox <- averagingFunc("oxygen.mg")
+
+#####################
+# AvgOneStationFunc
+#####################
+
+AvgOneStationFunc <- function(StationDataFrame) {
+  temp <- StationDataFrame
+  temp$pressure.round <- round(temp$pressure,1) # Round the pressure to one decimal place to reduce the amount of data and to standardize
+  temp$posixdate <- as.POSIXct(temp$date,format="%b %d %Y") # Create an actual date object
+
+  # Subset an individual cast and perform minor manipulations
+  # Create a list to put the data frames into
+  templist <- list()
+  turblist <- list()
+  oxlist <- list()
+  for (i in (1:length(levels(temp$date)))) {
+    onedate <- subset(temp,date==levels(temp$date)[i]) # Pull one date/cast
+    # Find the day number from the original temp data frame (one station is subsetted)
+    onedate.temp <- averagingFunc(column="temp.c",data=onedate) # Runs the function to create new a new data frame for one station and one date
+    onedate.turb <- averagingFunc("turb.ntu",onedate)
+    onedate.ox <- averagingFunc("oxygen.mg",onedate)
+    # Put the data frames into the list
+    templist[[i]] <- onedate.temp
+    turblist[[i]] <- onedate.turb
+    oxlist[[i]] <- onedate.ox
+  }
+  # Bind the dates into one frame for each variable 
+  AllTemp <- do.call("rbind",templist)
+  AllTurb <- do.call("rbind",turblist)
+  AllOx <- do.call("rbind",oxlist)
+  # browser()
+  rownames(AllTemp) <- 1:nrow(AllTemp)
+  rownames(AllTurb) <- 1:nrow(AllTurb)
+  rownames(AllOx) <- 1:nrow(AllOx)
+  AllTemp$station <- temp$station[1]
+  AllTurb$station <- temp$station[1]
+  AllOx$station <- temp$station[1]
   
-# Bind the data for each "month" together so that it is ready for plotting
+  # Put these three frames into one list
+  OneStationList <- list(Temp=AllTemp,Turb=AllTurb,Ox=AllOx)
+  return(OneStationList)
+}
+
+###########################################
+# Run averaging functions for all stations
+###########################################
+for (j in 1:length(file_path)) {
+  onestationdata <- read.csv(sprintf("StationDataCSV/%s",file_path[j]),head=TRUE) # This pulls one station from the data and it a repeat from the start of this file
+  name <- sprintf("Averaged%s",substr(file_path[j],start=1,stop=2))
+  stationdata <- AvgOneStationFunc(onestationdata)
+  assign(name,stationdata,envir=.GlobalEnv)
+}
+ 
+# The above loop has created a list with the name format "AveragedS1"
+names(AveragedS1)
+class(AveragedS1)
+class(AveragedS1[[1]])
+# You can double check that the code isn't accidentally writing over itself
+ddply(AveragedS1$Temp, c("month"), summarize, max.temp=max(temp.c))
+
+
+#!!!!!!!!! EDITED UP TO HERE
+
+######################################
+######### Plotting Data ##############
+######################################
+
+
+##########################
+# OneStationPlotFunc
+##########################
+
+# Code to plot data from an individual station (temp, oxygen, cond, turb, fluor)
+# Creates a new page (window) for each cast (i.e., date)
+
+OneStationPlotFunc <- function(StationDataFrame) {     # StationDataFrame is before the averaging manipulation. e.g., S1data, A1data, etc
+  castonly <- subset(StationDataFrame,portion=="cast") # Remove surface and retrieval portions
+  yaxisrange <- rev(range(castonly$pressure.db)) # Set the axes so that all of the graphs will be comparable
+  temprange <- range(castonly$temp.c)
+  condrange <- range(castonly$cond.us.cm3)
+  turbrange <- range(castonly$turb.ntu)
+  fluorrange <- range(castonly$fluor.mg.m3)
+  oxrange <- range(castonly$oxygen.mg)
+  for (i in 1:length(levels(castonly$date))) {   # Loop over each date/cast and create panel plot
+    cast <- subset(castonly,date==levels(castonly$date)[i])
+    windows()
+    par(mfrow=c(2,3),oma=c(1,1,2,1))
+    plot(cast$temp.c,cast$pressure.db,type="l",ylim=yaxisrange,xlim=temprange,ylab="Pressure dbar", xlab="Temp C")
+    plot(cast$cond.us.cm3,cast$pressure.db,type="l",ylim=yaxisrange,xlim=condrange,ylab="Pressure dbar", xlab="Conductivity")
+    plot(cast$turb.ntu,cast$pressure.db,type="l",ylim=yaxisrange,xlim=turbrange,ylab="Pressure dbar", xlab="Turbidity NTU")
+    plot(cast$fluor.mg.m3,cast$pressure.db,type="l",ylim=yaxisrange,xlim=fluorrange,ylab="Pressure dbar", xlab="Fluorescence mg/m3")
+    plot(cast$oxygen.mg,cast$pressure.db,type="l",ylim=yaxisrange,xlim=oxrange,ylab="Pressure dbar", xlab="Oxygen mg/L")
+    mtext(sprintf("%s: %s",cast$station[1],cast$date[1]), side = 3, line=0, outer = TRUE)
+  }
+}
+
+# Run example
+# Will automatically open new plot windows; one for each date
+OneStationPlotFunc(S1data)
+
+
+#################################
+# Heat Map Plots
+################################
+
